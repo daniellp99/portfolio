d16f51d9ea81bcef6ec7e1e61d8782e850bf0fd9
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-01-02 19:48:00 -0500

    fix: react-grid-layout import reference

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,7 +1,7 @@
 import NavBar from "@/components/NavBar";
 import { ThemeProvider } from "@/components/Providers";
 
-import "@/../../node_modules/react-grid-layout/css/styles.css";
+import "@/../node_modules/react-grid-layout/css/styles.css";
 import "./globals.css";
 
 export default function HomeLayout({
