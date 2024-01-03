74deb6c8a1bdc06ede1d5cb475d667327a7f2280
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-01-02 19:48:00 -0500

    fix: react-grid-layout import reference

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 import { Metadata } from "next";
 
+import "@/../node_modules/react-grid-layout/css/styles.css";
+
 export const metadata: Metadata = {
   title: "Daniel's Portfolio",
   description: "Developer from Cuba.",
