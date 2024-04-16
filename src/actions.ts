0d2c5357370f694e437845006ff69848cb03f07c
Daniel Alejandro Leyva Perez <82961309+daniellp99@users.noreply.github.com>
2024-04-15 23:20:22 -0400

    feat: server action for generate image layouts

--- src/actions.ts
+++ src/actions.ts
@@ -3,8 +3,10 @@
 import {
   AboutLayoutFactory,
   DefaultLayoutFactory,
+  ImageLayoutFactory,
   ProjectsLayoutFactory,
 } from "./data/grid-layouts";
+import { Images } from "./data/project-dto";
 import { TabsType } from "./types/tabs";
 
 const layoutFactories = {
@@ -15,6 +17,20 @@ const layoutFactories = {
 
 export async function generateLayouts(tab: TabsType) {
   const LayoutFactory = layoutFactories[tab] || new DefaultLayoutFactory();
+
+  const lgLayout = await LayoutFactory.generateLayout("lg");
+  const smLayout = await LayoutFactory.generateLayout("sm");
+  const xsLayout = await LayoutFactory.generateLayout("xs");
+
+  return {
+    lg: lgLayout,
+    md: lgLayout,
+    sm: smLayout,
+    xs: xsLayout,
+  };
+}
+export async function generateImageLayouts(images: Images) {
+  const LayoutFactory = new ImageLayoutFactory(images);
 
   const lgLayout = await LayoutFactory.generateLayout("lg");
   const smLayout = await LayoutFactory.generateLayout("sm");
